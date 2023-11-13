834dd0229ee7d4fefad8dc66cc0eba7587f2e1dd
helyahsiung <x2819733925@163.com>
2023-11-13 16:23:12 +0800

    display modify

--- main.py
+++ main.py
@@ -15,7 +15,15 @@ timesteps = 1000
 # viscosity - viscosity of the gas
 # windNoise - how much the wind speed direction is changed as the simulation is unrolled (in degrees)
 # windNoiseTimestep - how ofteh the wind speed direction is changed as the simulation is unrolled
-fs = FluidSimulation(64, windSpeed=0.1, windDirection = 60.0, gasLocationX=32, gasLocationY=32, gasRelease=300, diffusion=0.0001, viscosity=0, windNoise=180, windNoiseTimestep=30)
+fs = FluidSimulation(64, windSpeed=0.1,
+                     windDirection=60.0,
+                     gasLocationX=32,
+                     gasLocationY=32,
+                     gasRelease=300,
+                     diffusion=0.0001,
+                     viscosity=0,
+                     windNoise=180,
+                     windNoiseTimestep=30)
 
 
 # Save the data to a file
@@ -25,16 +33,22 @@ f = h5py.File("test.hdf5", "w")
 # Run the simulation
 for i in range(timesteps):
     fs.update(f)
-    print ("Sim: "+str(i))
+    print("Sim: "+str(i))
 
 # Load the data from the saved file
 file = h5py.File("test.hdf5", "r")
 
 # Visualize the data - every 10th image is shown
-for i in range(0,timesteps,10):
-    plt.clf()
+fig, ax = plt.subplots()
+plt.ion()
+for i in range(0, timesteps, 10):
     # Show the gas concetration as an image
-    plt.imshow(file['readings'+str(i)], vmin=0, vmax=10)
-    plt.colorbar()
-    plt.pause(0.000000001)
-    print ("Render: "+str(i))
+    sc = ax.imshow(file['readings'+str(i)], vmin=0, vmax=10)
+    if i == 0:
+        cbar = plt.colorbar(sc, ax=ax)
+    else:
+        pass
+    plt.pause(0.2)
+    ax.cla()
+    print("Render: "+str(i))
+plt.close(fig)
